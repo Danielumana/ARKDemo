c65d914d62fefe4ed2453147da69ac4882ea52a5
Danielumana <daniel.umana.lopez@gmail.com>
2024-10-23 18:52:21 -0500

    Added logic to get a constant velocity of the ball in every bounce

--- Assets/ARKProject/Scripts/Ball/BallBehaviour.cs
+++ Assets/ARKProject/Scripts/Ball/BallBehaviour.cs
@@ -3,13 +3,39 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
 
+    private GameObject ballReference;
+    private Rigidbody ballRigidbody;
+
+    public float desiredConstantSpeed = 10.0f;
     void Start()
     {
-        
+        ballReference = this.gameObject;
+        if (ballReference == null)
+        {
+            return;
+        }
+        ballRigidbody = ballReference.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        if (ballRigidbody == null)
+        {
+            return;
+        }
+        Debug.Log("Actual Velocity: "+ballRigidbody.linearVelocity);
     }
+
+    private void OnCollisionEnter(Collision otherCollision) 
+    {
+        if (ballRigidbody == null)
+        {
+            return;
+        }
+
+        Vector3 currentBallNormalizedVelocity = ballRigidbody.linearVelocity.normalized;
+        ballRigidbody.linearVelocity = currentBallNormalizedVelocity * desiredConstantSpeed;
+
+    }
+
 }
